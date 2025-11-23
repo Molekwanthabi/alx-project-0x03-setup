59ae43480ee802e9551bd96edc358e48d0f0e655
Nthabiseng Molekwa <molekwanthabiseng@gmail.com>
2025-11-23 12:08:12 +0200

    Update index.tsx

---
+++ alx-project-0x03/pages/index.tsx
@@ -0,0 +1,9 @@
+import { PageRouteProps } from "@/interfaces";
+
+export default function Home({}: PageRouteProps) {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Welcome to Next.js</h1>
+    </div>
+  );
+}
